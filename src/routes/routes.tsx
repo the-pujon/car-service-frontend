1c61b86e6c4ef769930c4af006427c97e09139b8
Pujon Das Auvi <auvipujondas@gmail.com>
2024-09-09 11:48:47 +0600

    sign in and sign up page added

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
+import SignUp from "@/pages/Authentication/SignUp";
+import SignIn from "@/pages/Authentication/SignIn";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +12,19 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />,
+            }
+        ]
+    },
+    {
+        path: "/auth",
+        children: [
+            {
+                path: "signup",
+                element: <SignUp />,
+            },
+            {
+                path: "signin",
+                element: <SignIn />,
             }
         ]
     }
