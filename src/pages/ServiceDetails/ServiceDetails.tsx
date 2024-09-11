5ae7cfbe6bc3c318b0726df8869a493a01fa27e1
Pujon Das Auvi <auvipujondas@gmail.com>
2024-09-11 10:50:49 +0600

    service details page refectored

--- src/pages/ServiceDetails/ServiceDetails.tsx
+++ src/pages/ServiceDetails/ServiceDetails.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Calendar } from '@/components/ui/calendar'
 import { Button } from '@/components/ui/button'
-import { Card,CardContent,CardDescription,CardFooter,CardHeader,CardTitle } from '@/components/ui/card'
+import { Card,CardContent,CardDescription,CardHeader,CardTitle } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
 import { format } from 'date-fns'
 //import { motion } from 'framer-motion'
