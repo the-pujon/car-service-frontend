1d51650561a534d2f9c1d13496842975aa0ee056
Pujon Das Auvi <auvipujondas@gmail.com>
2024-09-14 15:21:00 +0600

    baseApi refectored

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,61 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { toast } from "sonner";
+import { RootState } from "../store";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/apiw",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    console.log(token);
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithErrorHandling: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+> = async (args, api, extraOptions): Promise<any> => {
+  try {
+    const result = await baseQuery(args, api, extraOptions);
+
+    if (result.error) {
+      if (result.error.status === 401) {
+        console.error("Unauthorized access. Please check your credentials.");
+      } else if (result.error.status === 403) {
+        console.error(
+          "Forbidden. You do not have permission to access this resource."
+        );
+      } else if (result.error.status === 404) {
+        toast.error("Resource not found.");
+      } else {
+        console.error("An unexpected error occurred:", result.error);
+      }
+    }
+
+    return result;
+  } catch (error) {
+    console.error("An error occurred during the request:", error);
+    throw error;
+  }
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: baseQueryWithErrorHandling,
   endpoints: () => ({}),
 });
 
