93c1cf260fa825556cb1635643ec8d8cf359b7f0
Pujon Das Auvi <auvipujondas@gmail.com>
2024-11-04 03:04:56 +0600

    scrolling issue solved

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,6 +5,11 @@ import { useEffect } from "react";
 
 const MainLayout = () => {
 
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0,0);
+    },[pathname]);
 
 
     return (
