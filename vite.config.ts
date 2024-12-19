42e00d16818f91280bc6f41f3d162fbeca443ab3
Pujon Das Auvi <auvipujondas@gmail.com>
2024-12-20 01:12:41 +0600

    feat(build): optimize bundle splitting and development config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,25 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
+          'redux': ['@reduxjs/toolkit', 'react-redux'],
+          'ui': ['@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    open: true,
+    port: 5173,
   },
 });
